6aa9a4a26c256ef23331a46b32fd7d509056e314
MiSAKASiRiN <1617297875@qq.com>
2021-06-07 01:26:36 +0800

    CORE Library for Cpp -UNFINISHED

---
+++ CORE/main.cpp
@@ -0,0 +1,17 @@
+/*
+################################################################
+#                                                              #
+#     MisakaSirin                                              #
+#     NMEA-Process CORE for Cpp (Library)                      #
+#     Version 0.0.0@000000 Build 0                             #
+#     Please keep the source for reprint                       #
+#     -> github.com/MiSAKASiRiN                                #
+#     Author Contact: ohhh@wuzehao.pro                         #
+#     Free software, please do not use for commercial purposes #
+#                                                              #
+################################################################
+*/
+
+//###FILE BEGIN###
+
+//###FILE END###
